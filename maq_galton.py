d39c610df09c7b0a6d2a3e94ecd68cf67aaba938
YafteBuenoL <134566047+YafteBuenoL@users.noreply.github.com>
2023-07-28 20:32:50 +0000

    se empezo proyecto

---
+++ maq_galton.py
@@ -0,0 +1,29 @@
+import matplotlib as plt
+import numpy as np
+import random as randint
+
+print("Desea realizar una prueba de la maquina de galton")
+
+try:
+    opcion = ("SI ejecutar programa (1)/NO ejecutar (2) ")
+except:
+    print("Ingrese un numero valido")
+    continue
+
+if opcion == 1
+    #se ejecutara el programa
+    print("Se dejo caer 3000 pelotas por la maquina de Galton de 12 niveles")
+    niveles = 12
+    casillas = 100
+    canicas = 3000
+    
+
+
+elif opcion == 2
+    print("Fin de el programa")
+    break
+else:
+    print("Solo elige opcion 1 o 2")
+    continue
+
+
